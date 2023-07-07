d794a4c548e685b02f5c26221fcb3f288bf58551
wuminglangren <wuminglangren@163.com;wuminglangren03@gmail.com;wuminglangren03@outlook.com>
2023-07-07 17:50:06 +0000

    I found some fonts that are used for websites displaying, and I need to write a script to take them out to the specific directory for following steps.

---
+++ database/fonts/cores/collect_core_fonts.py
@@ -0,0 +1,14 @@
+# collect important files which are used on website most
+# 
+# -----FONT NAMES-----
+# BlinkMacSystemFont,
+# Helvetica Neue,
+# Helvetica,
+# Arial,
+# PingFang SC,
+# Hiragino Sans GB,
+# Microsoft YaHei,
+# Source Han Sans SC, need afdko
+# Noto Sans CJK SC,
+# WenQuanYi Micro Hei,
+# sans-serif
